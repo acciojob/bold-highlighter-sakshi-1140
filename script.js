805780f9346347ac000e697602e96ddcf7edd283
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-25 19:47:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
-
-
+    let words=document.querySelectorAll('strong');
+    //console.log(words[0]);
+    //console.log(words.length);
+    words.forEach(elem=>{
+        elem.style.color="green";
+    })
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+    let words=document.querySelectorAll('strong');
+    //console.log(words[0]);
+    //console.log(words.length);
+    words.forEach(elem=>{
+        elem.style.color="black";
+    })
 }
+    
